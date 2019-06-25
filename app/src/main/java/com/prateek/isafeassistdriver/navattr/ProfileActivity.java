627d43889ec8d53f16ad0386b2af6fd5a984f664
Prateek Aggarwal <aggarwalprateek72@gmail.com>
2019-06-25 11:47:58 +0530

    app logo added

--- app/src/main/java/com/prateek/isafeassistdriver/navattr/ProfileActivity.java
+++ app/src/main/java/com/prateek/isafeassistdriver/navattr/ProfileActivity.java
@@ -2,7 +2,13 @@ package com.prateek.isafeassistdriver.navattr;
 
 import android.app.ActionBar;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,17 +18,30 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.prateek.isafeassistdriver.R;
 import com.prateek.isafeassistdriver.dao.Driver;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,11 +51,17 @@ public class ProfileActivity extends AppCompatActivity {
     Driver driver;
     CircleImageView profileimage;
     FirebaseAuth auth;
+    CircleImageView profile;
     Button edit;
     TextView username;
     DatabaseReference databaseReference;
     ProgressDialog progressDialog;
     ActionBar actionBar;
+    ProgressBar progressBar;
+    Uri filepath;
+    private static final int PICK_IMAGE_REQ = 71;
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
 
     @Override
@@ -47,12 +72,15 @@ public class ProfileActivity extends AppCompatActivity {
         email = findViewById(R.id.user_emailid);
         username = findViewById(R.id.userprofile_username);
         edit = findViewById(R.id.edit_profile_btn);
+        profile = findViewById(R.id.user_circular_profile);
         Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Profile");
         edit.setText("Edit");
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
         driver = new Driver();
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
@@ -82,10 +110,11 @@ public class ProfileActivity extends AppCompatActivity {
                     databaseref.child(auth.getCurrentUser().getUid()).child("mail").setValue(email.getText().toString());
                     databaseref.child(auth.getCurrentUser().getUid()).child("contact").setValue(mobileno.getText().toString());
 
+                    uploadPicture();
                     System.out.println(email.getText().toString());
                     System.out.println(mobileno.getText().toString());
 
-                    Toast.makeText(ProfileActivity.this,"Profile Updated Successfully",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ProfileActivity.this, "Profile Updated Successfully", Toast.LENGTH_SHORT).show();
                     mobileno.setEnabled(false);
                     email.setEnabled(false);
                     edit.setText("Edit");
@@ -94,6 +123,63 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateDp();
+            }
+        });
+    }
+
+    private void uploadPicture() {
+        if (filepath != null) {
+            final StorageReference ref = storageReference.child("images/" + UUID.randomUUID().toString());
+            ref.putFile(filepath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+
+                                    String url = uri.toString();
+                                    System.out.println(url);
+                                    Glide.with(ProfileActivity.this).load(url).into(profile);
+
+                                }
+                            });
+                            Toast.makeText(ProfileActivity.this, "Successfully added", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        }
+    }
+
+    private void updateDp() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQ);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQ && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filepath = data.getData();
+            try {
+
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
+                profile.setImageBitmap(bitmap);
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
     }
 
     private void loadinfo() {
